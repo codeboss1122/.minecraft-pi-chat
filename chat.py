2fff8abe7add54d62a79887f5a85170f496416de
codeboss1122 <78480810+codeboss1122@users.noreply.github.com>
2021-02-10 10:38:43 +0000

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,8 +1,12 @@
 from mcpi.minecraft import Minecraft
-
+import time
 l = ','
 
+
+
 n = (input('what is your minecraft username? >'))
+k = 4
+
 
 b = 1
 while True:
@@ -13,4 +17,35 @@ while True:
 
      c = (n,  l,  i)
 
+     time.sleep(3)
+
      mc.postToChat(c)
+
+     mca = Minecraft.create()
+
+     inp = (input('>'))
+
+     ca = (n,  l,  inp)
+
+     time.sleep(3)
+
+     mca.postToChat(ca)
+
+     mcb = Minecraft.create()
+
+     inpu = (input('>'))
+
+     cb = (n,  l,  i)
+     
+     time.sleep(3)
+
+     mcb.postToChat(cb)
+     j = "was temp muted for spamming" 
+     no = (n, ",,",  j, ",", inp)
+
+     if i == inp == inpu:
+         mc.postToChat(no)
+         break
+
+
+
